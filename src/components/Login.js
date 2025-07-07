f4c9f4b1516f476ea818f09820ac60b535f4a50c
top-notch8 <rajisholaa@gmail.com>
2025-07-08 00:33:08 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,8 @@ const Login = ({ onLogin }) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:8000/api/auth/login', {
+      const res = await axios.post('axios.post('https://mentoapp-backend.onrender.com/api/auth/login', ...)
+', {
         email,
         password
       });
